c87ee4662e92e042720db3a9a477a51fa137be37
Apoorv <vaporinside@gmail.com>
2022-05-27 07:47:58 +0530

    log27/05/2022

---
+++ update.py
@@ -0,0 +1,17 @@
+import datetime
+import subprocess
+
+time = datetime.datetime.now().strftime('%d/%m/%Y')
+
+comment = input("Enter today's update: \n")
+comment = f"{time} | {comment}"
+
+file = open("log.txt", 'a')
+file.write(comment)
+file.close()
+
+commit_msg = "log" + time
+
+subprocess.call(["git", "add", "."])
+subprocess.call(["git", "commit", "-m", commit_msg])
+subprocess.call(["git", "push"])
